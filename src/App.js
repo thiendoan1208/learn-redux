fd858804ee576a4871d0e5d3aba61824becb92bc
thiendoan1208 <thienmacro1234@gmail.com>
2025-02-13 10:37:53 -0500

    setup reduxredux

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { connect } from 'react-redux';
+
+import { increaseCounter, decreaseCounter } from './action/actions';
+
+function App(props) {
+  return (
+    <>
+      <div>Count: {props.count}</div>
+
+      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+
+      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+    </>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.counter.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+
+    decreaseCounter: () => dispatch(decreaseCounter()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
