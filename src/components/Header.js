fd1494057682969711b7224d122ebc76733fc1be
thiendoan1208 <thienmacro1234@gmail.com>
2025-02-24 13:51:18 -0500

    Counter users

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useSelector} from 'react-redux';
 
 function Header() {
+  const listUsers = useSelector((state) => state.user.listUser);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -13,7 +16,7 @@ function Header() {
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown title={`(${listUsers.length}) Users`} id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
